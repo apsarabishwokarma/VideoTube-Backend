3ba9a8faed261bd28e6ad74229400222966e7568
apsarabishwokarma <apsara.devx@gmail.com>
2026-06-15 15:38:15 +0545

    feat: router and controller config

--- src/app.js
+++ src/app.js
@@ -28,4 +28,12 @@ app.use(express.static("public"));
 
 app.use(cookieParser());
 
+//routes import
+import userRouter from "./routes/user.routes.js";
+
+// const Api = app.use("/api/v1")
+
+//routes declaration
+app.use("/api/v1/users", userRouter);
+
 export default app;
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -27,7 +27,7 @@ import { message } from "statuses";
 //2.PROMISES THEN AND CATCH
 
 const asyncHandler = (requestHandler) => {
-  (req, res, next) => {
+  return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
   };
 };
